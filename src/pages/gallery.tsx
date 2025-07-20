61f89fa4fe5b50e9ccabd45bc5790ff6cd06385d
Goddy01 <adigungodwin2@gmail.com>
2025-07-20 01:04:41 +0100

    updated

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -65,16 +65,7 @@ const PhotoCard = ({ photo }: { photo: GalleryPhoto }) => {
     }
   }
 
-  const formatDate = (dateString: string) => {
-    const date = new Date(dateString)
-    return date.toLocaleDateString('en-US', {
-      year: 'numeric',
-      month: 'short',
-      day: 'numeric',
-      hour: '2-digit',
-      minute: '2-digit',
-    })
-  }
+
 
   return (
     <motion.div
